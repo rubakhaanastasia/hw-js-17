ca36258b5fc990b1a778c8f9fb3cec3637dce8fe
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2025-02-01 13:12:16 +0100

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,28 @@
+//1.
+const buttonElement = document.querySelector(".button");
+console.log(buttonElement);
+const textElement = document.querySelector(".text");
+console.log(textElement);
+buttonElement.textContent = textElement.textContent;
+
+//2.
+const imgElement = document.querySelector(".img");
+console.log(imgElement);
+imgElement.src =
+    "https://i.pinimg.com/736x/1b/d1/ed/1bd1ed1e8a23a3151e6c6b867e1402e9.jpg";
+  
+//3.
+const linkElement = document.querySelector(".link");
+console.log(linkElement);
+linkElement.setAttribute = "https://github.com/rubakhaanastasia";
+
+const imgElement = document.querySelector(".img");
+console.log(imgElement);
+imgElement.src = 
+    "https://i.pinimg.com/736x/1b/d1/ed/1bd1ed1e8a23a3151e6c6b867e1402e9.jpg";
+  
+
+//4.
+const listElement = document.querySelector(".list");
+console.log(listElement);
+
